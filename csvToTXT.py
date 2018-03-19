df139b7392f7d8d362b0ca89798d1b685d8b36c5
SL34K <30938710+SL34K@users.noreply.github.com>
2018-03-19 22:54:56 +0000

    Add files via upload

---
+++ csvToTXT.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#.------..------..------..------..------.
+#|S.--. ||L.--. ||3.--. ||4.--. ||K.--. |
+#| :/\: || :/\: || :(): || :/\: || :/\: |
+#| :\/: || (__) || ()() || :\/: || :\/: |
+#| '--'S|| '--'L|| '--'3|| '--'4|| '--'K|
+#`------'`------'`------'`------'`------'
+#https://twitter.com/SL34K
+#https://github.com/SL34K
+#00110001 00110011
+#00110000 00110011
+#00110001 00111000 
+#########################################
+import csv
+f = open('100_followers.csv', 'rb')
+csv_row = []
+usernames = []
+reader = csv.reader("100_followers.csv", delimiter="\n")
+try:
+    for line,line_num in enumerate(open("100_followers.csv", 'rb')):
+        csv_row.append(line_num.split())
+        test = csv_row[line][0].decode('UTF-8')
+        usernames.append((test.split(',')[1][1:-1]))
+except:
+    f = open("usernames.txt",'a')
+    for i in usernames:
+        f.write(i+'\n')
+    f.close() 
+    print("Saved to usernames.txt")
+
